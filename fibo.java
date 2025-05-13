f87d4f32d25f7e82c52691f1b504ff39d7389686
ruhii01 <ruhaniroshan01@gmail.com>
2025-05-13 14:38:47 +0530

    Fibonacci series

---
+++ fibo.java
@@ -0,0 +1,14 @@
+public class fibo {
+
+    public static void main(String[] args) {
+        int n=10;
+        int a=6,b=1;
+        System.out.println("fibonacci series upto"+ n +"terms");
+        for(int i=1;i<n;i++){
+            System.out.println(a+" ");
+            int next=a+b;
+            a=b;
+            b=next;
+        }
+    }
+}
